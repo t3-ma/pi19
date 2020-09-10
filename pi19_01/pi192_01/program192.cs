cd8b7271c9bf6d2eee2d240236d58265295fee21
user <user@PC>
2020-09-10 20:34:33 +0700

    initial commit

---
+++ pi19_01/pi192_01/program192.cs
@@ -0,0 +1,83 @@
+﻿#region usings
+using System;
+#endregion
+
+namespace pi192_01
+{
+  class program192
+  {
+    private static void Main(string[] args)
+    {
+      string sText = "Текст длинный или не очень " +
+          ", а даже очень!";
+      string sLongestWord = h_GetLongWord(sText);
+      //      Console.WriteLine("Проанализировали строку \"" + sText + "\"");
+      //      Console.WriteLine("Самое длинное слово \"" + sLongestWord + "\"");
+      Console.WriteLine($"Проанализировали строку \"{sText}\"");
+      Console.WriteLine($"Самое длинное слово \"{sLongestWord}\"");
+      Console.ReadKey();
+    }
+
+    private static string h_GetLongWord(
+        string sText)
+    {
+      int iLen = sText.Length;
+      int iPos = 0;
+      int iCurrentWordLen = 0;
+      int iMaxLenWord = 0;
+      string sMaxLenWord = String.Empty;
+      while (iPos < iLen)
+      {
+        char ch = sText[iPos];
+        if (!h_IsCharDivider(ch))
+        {
+          iCurrentWordLen++;
+        }
+        else
+        {
+          string sCurrentWord =
+            sText.Substring(
+              iPos - iCurrentWordLen,
+              iCurrentWordLen);
+
+          if (iMaxLenWord < iCurrentWordLen)
+          {
+            iMaxLenWord = iCurrentWordLen;
+            sMaxLenWord = sCurrentWord;
+          }
+          iCurrentWordLen = 0;
+        }
+
+        iPos++; //  iPos = iPos + 1;
+      }
+      return sMaxLenWord;
+    }
+
+    private static bool h_IsCharDivider(char ch)
+    {
+      char[] arDelimiters = // new char[5]
+        { ',', '.', ' ', ';', '-' };
+
+      //int ii = 0; // инициализатор
+      //while (ii < arDelimiters.Length)
+      //{ // условие продолжения
+
+      //  // ....
+
+      //  ii++; // постоперация
+      //}
+      for (
+        int ii = 0; // инициализатор
+        ii < arDelimiters.Length; // условие продолжения
+        ii++ // постоперация
+      )
+      {
+        if (ch == arDelimiters[ii])
+        {
+          return true;
+        }
+      }
+      return false;
+    }
+  }
+}
---
+++ pi19_01/pi193_01/Program.cs
@@ -0,0 +1,117 @@
+﻿#region usings
+using System;
+#endregion
+
+namespace pi193_01
+{
+  internal class Program193
+  {
+
+    #region pi193
+    private static void Main(string[] args)
+    {
+
+      string sText = "Самый длинн----ый день в году именно сегодня";
+      string sLongestWord = h_FindLongestWord(sText);
+      string sHasDigits = h_FindDigitsWord(sText);
+
+      //Console.WriteLine(sText + " - это текст, который мы анализировали");
+      //Console.WriteLine("<" + sLongestWord + "> слово, которое в нем самое длинное");
+      Console.WriteLine($"{sText} - это текст, который мы анализировали");
+      Console.WriteLine($"<{ sLongestWord }> слово, которое в нем самое длинное");
+      Console.ReadKey();
+    }
+
+    private static string h_FindDigitsWord(string sText)
+    {
+      int iPos = 0;
+      int iCurrentLen = 0;
+      bool bIsDigitWord = false;
+
+      while (iPos < sText.Length)
+      {
+        if (!h_IsDivider(sText[iPos]))
+        {
+          if (Char.IsDigit(sText[iPos]))
+          {
+            bIsDigitWord = true;
+          }
+          iCurrentLen++;
+        }
+        else
+        {
+
+          if (bIsDigitWord)
+          {
+            return sText.Substring(iPos - iCurrentLen, iCurrentLen);
+          }
+          iCurrentLen = 0;
+          bIsDigitWord = false;
+        }
+        iPos++;
+      }
+
+      return String.Empty;
+    }
+
+    private static string h_FindLongestWord(string sText)
+    {
+      int iPos = 0;
+      int iCurrentLen = 0;
+      int iMaxLen = 0;
+      string sMaxWord = "";
+
+      while (iPos < sText.Length)
+      {
+        if (!h_IsDivider(sText[iPos]))
+        {
+          iCurrentLen++;
+        }
+        else
+        {
+          if (iCurrentLen > iMaxLen)
+          {
+            iMaxLen = iCurrentLen;
+            sMaxWord = sText.Substring(iPos - iCurrentLen, iCurrentLen);
+          }
+
+          iCurrentLen = 0;
+        }
+        iPos++;
+      }
+
+      return sMaxWord;
+
+    }
+
+    private static bool h_IsDivider(char c)
+    {
+      char[] arDividers = new char[] { ',', '.', ' ' };
+      for (
+        int ii = 0;               // инициализатор 
+        ii < arDividers.Length;   // проверка предусловия
+        ii++                      // поствыражение
+      )
+      {
+        char ch = arDividers[ii];
+        if (ch == c)
+        {
+          return true;
+        }
+      }
+
+      foreach (char ch in arDividers)
+      {
+        if (ch == c)
+        {
+          return true;
+        }
+      }
+
+
+      return false;
+    }
+
+    #endregion
+  }
+}
