041d4c8300bebecd7d8cf0b9b49b5584bdadb36e
Sergey A. Verevkin <sergeyverevkin@gmail.com>
2020-10-17 11:59:44 +0700

    2020-10-17

--- pi19_01/pi191_03CA/pi191_03CA/Program.cs
+++ pi19_01/pi191_03CA/pi191_03CA/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using pi191_03CL.Model;
+using pi191_03CL.Model2;
 
 namespace pi191_03CA
 {
@@ -11,16 +12,39 @@ namespace pi191_03CA
   {
     static void Main(string[] args)
     {
+
+      // h_TestPolymorph();
+      h_TestMailServer();
+
+      Console.ReadKey();
+    }
+
+    private static void h_TestMailServer()
+    {
       TestMailServer pMailServer = new TestMailServer("mail.ru");
 
       int ii = 0;
-      foreach (Mailbox pMb in pMailServer.MailboxList) {
+      foreach (Mailbox pMb in pMailServer.MailboxList)
+      {
         Console.WriteLine((++ii).ToString());
         Console.WriteLine(pMb.Address);
         Console.WriteLine($"{pMb.GetUnreadCount()} / {pMb.LetterList.Count}");
       }
+    }
 
-      Console.ReadKey();
+    private static void h_TestPolymorph()
+    {
+      Animal pAnimal;
+      pAnimal = new Cow();
+      h_MakeNoise(pAnimal);
+      pAnimal = new Parrot();
+      h_MakeNoise(pAnimal);
+    }
+
+    private static void h_MakeNoise(Animal pAnimal)
+    {
+      Console.WriteLine($"{pAnimal.GetType().Name}: {pAnimal.Say()}");
+      Console.WriteLine($"{pAnimal.GetType().Name}: {pAnimal.Say("R!")}");
     }
   }
 }
---
+++ pi19_01/pi191_03CL/pi191_03CL/Model2/Parrot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pi191_03CL.Model2
+{
+  public class Parrot: Animal
+  {
+    public override string Say()
+    {
+      return "R";
+    }
+
+    public override string Say(string sWord)
+    {
+      return sWord;
+    }
+  }
+}
