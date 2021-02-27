92b31ff5693538f49106c4949f64638641f0620f
Sergey Verevkin <verevkin@gnivc.ru>
2021-02-27 08:40:34 +0700

    L1_WCF

---
+++ pi19_02/L1_WCF/L1.WcfServiceLibrary/EncyclopediaService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace L1.WcfServiceLibrary
+{
+  public class EncyclopediaService : IEncyclopediaService
+  {
+  
+    /// <summary>
+    /// Получение списка категорий и информации о энциклопедии
+    /// </summary>
+    /// <returns></returns>
+    public EncyclopediaType GetInfo()
+    {
+      // TODO
+      return null;
+    }
+
+    /// <summary>
+    /// Получить информацию по разделу энциклопедии
+    /// </summary>
+    /// <param name="sCode"></param>
+    /// <returns></returns>
+    public EncyclopediaPartType GetPart(string sCode)
+    {
+      // TODO
+      return null;
+    }
+
+    /// <summary>
+    /// Получить полную словарную статью
+    /// </summary>
+    /// <param name="sCode"></param>
+    /// <returns></returns>
+    public EncyclopediaArticleType GetArticle(string sCode)
+    {
+      // TODO
+      return null;
+    }
+  }
+}
---
+++ pi19_02/L1_WCF/L1.WcfServiceLibrary/IEncyclopediaService.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace L1.WcfServiceLibrary
+{
+  /// <summary>
+  /// Сервис "Электронная энциклопедия"
+  /// </summary>
+  [ServiceContract]
+  public interface IEncyclopediaService
+  {
+    /// <summary>
+    /// Получение списка категорий и информации о энциклопедии
+    /// </summary>
+    /// <returns></returns>
+    [OperationContract]
+    EncyclopediaType GetInfo();
+
+    /// <summary>
+    /// Получить информацию по разделу энциклопедии
+    /// </summary>
+    /// <param name="sCode"></param>
+    /// <returns></returns>
+    [OperationContract]
+    EncyclopediaPartType GetPart(string sCode);
+
+    /// <summary>
+    /// Получить полную словарную статью
+    /// </summary>
+    /// <param name="sCode"></param>
+    /// <returns></returns>
+    [OperationContract]
+    EncyclopediaArticleType GetArticle(string sCode);
+  }
+
+  /// <summary>
+  /// Энциклопедия
+  /// </summary>
+  [DataContract]
+  public class EncyclopediaType
+  {
+    /// <summary>
+    /// Название энциклопедии
+    /// </summary>
+    [DataMember]
+    public string Title { get; set; }
+
+    /// <summary>
+    /// Список разделов энциклопедии
+    /// </summary>
+    [DataMember]
+    public EncyclopediaPartType[] PartList { get; set; }
+
+    // TODO
+  }
+
+  /// <summary>
+  /// Раздел энциклопедии
+  /// </summary>
+  public class EncyclopediaPartType
+  {
+    /// <summary>
+    /// Список разделов энциклопедии
+    /// </summary>
+    [DataMember]
+    public EncyclopediaArticleInfoType[] ArticleInfoList { get; set; }
+
+    // TODO
+  }
+
+  /// <summary>
+  /// Краткая информация о статье энциклопедии
+  /// </summary>
+  public class EncyclopediaArticleInfoType
+  {
+    // TODO
+  }
+  
+  /// <summary>
+  /// Полная статья энциклопедии с иллюстрацией
+  /// </summary>
+  public class EncyclopediaArticleType
+  {
+    // TODO
+  }
+}
