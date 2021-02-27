79964b4b51520e3e5c5de3f685e925ad0802e7f2
Administrator <Administrator@PRPO-BUILD>
2021-02-27 10:34:21 +0700

    manager

---
+++ pi19_02/L1_WCF/L1.WcfServiceLibrary/Manager/EncyclopediaManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1.WcfServiceLibrary.Manager
+{
+  /// <summary>
+  /// менеджер энциклопедий
+  /// </summary>
+  public class EncyclopediaManager
+  {
+    public EncyclopediaType Load(string sFilename)
+    {
+      EncyclopediaType pEncyclopedia = new EncyclopediaType();
+      pEncyclopedia.Title = "Пословицы и поговорки";
+      EncyclopediaPartType pPart1 = new EncyclopediaPartType();
+      // ...
+      pEncyclopedia.PartList = new[] {pPart1};
+    }
+  }
+}
